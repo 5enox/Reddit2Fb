abbde972f3f3b9a979b8f78de363d35372f0b331
5enox <animikantan@gmail.com>
2024-08-14 17:07:23 +0100

    File Structure

---
+++ main.py
@@ -0,0 +1,9 @@
+# from utils.reddit import ...
+# from utils.facebook import ...
+
+
+# if __name__ == '__main__':
+#     reddit = Reddit()
+#     facebook = Facebook()
+#     reddit.get_posts()
+#     facebook.post_to_page()
